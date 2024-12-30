68a0668d84075e213e139f10bd3a7c044f870ea1
Carson Davis <cmd52465@gmail.com>
2024-12-29 22:16:13 -0800

    redirect to dashboard from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return <Button>Click me</Button>;
+  return redirect("/dashboard");
 }
